a9e3cd40d80021adedb00de9543a6e40d20dce2f
umarali-nagoor <umarali.nagoor@in.ibm.com>
2021-03-29 21:00:49 +0530

    Test

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "ibm" {
+}
+
+data "ibm_resource_group" "cos_group" {
+  name = var.resource_group
+}
+
+module "cos" {
+
+  source = "terraform-ibm-modules/cos/ibm//modules/instance"
+
+  service_name      = var.service_name
+  resource_group_id = data.ibm_resource_group.cos_group.id
+  plan              = var.plan
+  region            = var.region
+}
