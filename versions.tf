455b15fab53d8be8dbb8e6a95571efb7b29caa17
umarali-nagoor <umarali.nagoor@in.ibm.com>
2021-03-29 21:09:29 +0530

    Added tf files

---
+++ versions.tf
@@ -0,0 +1,12 @@
+#####################################################
+# COS Instance
+# Copyright 2020 IBM
+#####################################################
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+    }
+  }
+}
