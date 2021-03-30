9004e5f2b11c258ad3c786340958d7b2975e9f14
umarali-nagoor <umarali.nagoor@in.ibm.com>
2021-03-30 10:14:44 +0530

    Added pre-commit file

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "cos" {
 
   source = "terraform-ibm-modules/cos/ibm//modules/instance"
 
-  service_name      = "S1"
+  service_name   = "S1"
   resource_group_id = data.ibm_resource_group.cos_group.id
   plan              = "standard"
   region            = "us-south"
