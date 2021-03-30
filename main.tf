dbdcaa5ffa43389e7c049ff8d05500105dfb111b
umarali-nagoor <umarali.nagoor@in.ibm.com>
2021-03-30 12:45:58 +0530

    Testing github action workflows

--- main.tf
+++ main.tf
@@ -8,8 +8,7 @@ data "ibm_resource_group" "cos_group" {
 module "cos" {
 
   source = "terraform-ibm-modules/cos/ibm//modules/instance"
-
-  service_name   = "S1"
+  service_name      = "S1"
   resource_group_id = data.ibm_resource_group.cos_group.id
   plan              = "standard"
   region            = "us-south"
--- variables.tf
+++ variables.tf
@@ -6,19 +6,23 @@
 variable "resource_group" {
   description = "Enter Name of the resource group"
   type        = string
+  default = "Default"
 }
 
 variable "service_name" {
   description = "Enter Name of the cos instance"
   type        = string
+  default = "S1"
 }
 
 variable "plan" {
   description = "Enter plan type"
   type        = string
+  default = "standard"
 }
 
 variable "region" {
   description = " Enter Region for provisioning"
   type        = string
+  default = "us-south"
 }
