ecde54c01700df0765195be310c0fc18b0c3efe8
umarali-nagoor <umarali.nagoor@in.ibm.com>
2021-03-30 15:42:34 +0530

    Removed .terraform files

--- examples/c2/.terraform/modules/cos/modules/instance/output.tf
+++
@@ -1,9 +0,0 @@
-#####################################################
-# COS Instance
-# Copyright 2020 IBM
-#####################################################
-
-output "cos_instance_id" {
-  description = "The ID of the cos instance"
-  value = length(ibm_resource_instance.cos_instance) > 0 ? ibm_resource_instance.cos_instance[0].id : ""
-}
